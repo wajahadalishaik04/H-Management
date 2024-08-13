0b1de65e95574477e18ba9d8d2c62cad38708f27
wajahadalishaik04 <wajahadalishaik@gmail.com>
2024-08-13 12:37:16 +0530

    changes to website name on signup page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
   return (
     <section class="bg-gray-700  ">
       <div class="flex flex-col items-center justify-center   px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <span className="bg-gradient-to-r from-red-500 to-white bg-clip-text text-transparent text-4xl mb-4 font-serif ">HS-management</span>
+        <span className="bg-gradient-to-r from-red-500 to-white bg-clip-text text-transparent text-4xl mb-4 font-serif ">H-management</span>
         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6  space-y-4 md:space-y-6 sm:p-8">
             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
